c8e4f84b53afe7a3e282fc8653b51b2c73035dbc
chexter@edau15 <tos515541905@gmail.com>
2020-11-19 01:27:04 +0800

    [PA2] top-down with self-defined class archived ~7m user time and 35.7gb mem

--- PA2/pa2/utilities/run.sh
+++ PA2/pa2/utilities/run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 rm outputs/*
-echo -n "" > log
-n=(10 20 50 100 200 500 1000 2000 5000 10000 20000 50000)
+echo -n "" > $1 
+n=(10 20 50 100 200 500 1000 2000 5000 10000 20000 50000 100000 150000 180000)
 for i in "${n[@]}"; do
-	python utilities/in_gen.py $i "inputs/$i"
-	echo -n "$i, " >> log
-	/usr/bin/time -f "%U" bin/mps "inputs/$i" "outputs/$i" &>> log
+	python3 utilities/in_gen.py $i "inputs/$i"
+	echo "$i"
+	echo -n "$i, " >> $1
+	/usr/bin/time -f "%e, %U, %M" bin/mps "inputs/$i" "outputs/$i" &>> $1
 done
 
